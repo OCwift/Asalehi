73755c172d70e9bdb0a97c139b77b245abdd00bc
OCwift <57847304+OCwift@users.noreply.github.com>
2019-11-16 14:50:43 -0800

    Add files via upload
    Building your First IOS 13 App

---
+++ ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  NTest1 11.1.19
+//
+//  Created by Ardalan Salehi on 11/2/19.
+//  Copyright © 2019 Ardalan, inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let DATA_KEY = "data_key"
+    @IBOutlet weak var textView: UITextView!
+    var pastedString : [String] = []
+    override func viewDidLoad() {
+        textView.text = "Hello!"
+        if let loadedString = UserDefaults.standard.string(forKey:DATA_KEY ) {
+            pastedString.append(loadedString)
+        }
+        showText()
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func addText(){
+        guard let text = UIPasteboard.general.string, !pastedString.contains(text) else{
+            return
+        }
+        
+        pastedString.append(text)
+        UserDefaults.standard.set(pastedString, forKey: DATA_KEY)
+        
+        showText()
+        
+    }
+    func showText(){
+        textView.text = ""
+        for str in pastedString {
+            textView.text.append("\(str)\n\n")
+        }
+        
+    }
+    @IBAction func trashWasPressed(_ sender: Any) {
+        pastedString.removeAll()
+        textView.text = ""
+        UserDefaults.standard.removeObject(forKey:DATA_KEY )
+    }
+    
+}
+
